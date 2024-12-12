fee05e42f5a571fea655fc66fad48506912bee91
KerQzi <jadikovkyrgyz@gmail.com>
2024-12-12 18:54:33 +0600

    update

--- app/src/main/java/com/example/noteapp/ui/fragments/onboard/OnBoardPagingFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/fragments/onboard/OnBoardPagingFragment.kt
@@ -1,54 +1,51 @@
 package com.example.noteapp.ui.fragments.onboard
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager2.widget.ViewPager2
+import androidx.fragment.app.Fragment
 import com.example.noteapp.R
 import com.example.noteapp.databinding.FragmentOnBoardPagingBinding
-import com.example.noteapp.ui.adapters.OnBoardViewPagerAdapter
 
 class OnBoardPagingFragment : Fragment() {
 
     private lateinit var binding: FragmentOnBoardPagingBinding
+    companion object{
+        const val ARG_ONBOARD_POSITION = "onboard_position"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentOnBoardPagingBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initialize()
-        setUpListener()
     }
 
-    private fun initialize() {
-        binding.viewPager.adapter = OnBoardViewPagerAdapter(this)
-        binding.dotsIndicator.attachTo(binding.viewPager)
-    }
-
-    private fun setUpListener() = with(binding.viewPager) {
-        registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                if (position == 2) {
-                    binding.tvSkip.visibility = View.INVISIBLE
-                    binding.btnStart.visibility = View.VISIBLE
-                }else{
-                    binding.tvSkip.visibility = View.VISIBLE
-                    binding.btnStart.visibility = View.GONE
-                    binding.tvSkip.setOnClickListener{
-                        setCurrentItem(currentItem + 2, true)
-                    }
-                }
+    private fun initialize() = with(binding) {
+        when(requireArguments().getInt(ARG_ONBOARD_POSITION)){
+            0 -> {
+                tvDescriptionTitle.text = "Удобство"
+                tvDescription.text = "Создавайте заметки в два клика! Записывайте мысли, идеи и важные задачи мгновенно."
+                lottieAnimationView.setAnimation(R.raw.first_anim)
             }
-        })
+            1 -> {
+                tvDescriptionTitle.text = "Организация"
+                tvDescription.text = "Организуйте заметки по папкам и тегам. Легко находите нужную информацию в любое время."
+                lottieAnimationView.setAnimation(R.raw.second_anim)
+            }
+            2 -> {
+                tvDescriptionTitle.text = "Синхронизация"
+                tvDescription.text = "Синхронизация на всех устройствах. Доступ к записям в любое время и в любом месте."
+                lottieAnimationView.setAnimation(R.raw.third_anim)
+            }
+        }
     }
 }
